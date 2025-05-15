7648d74dbc638b14f8a5f86ab49f0b393d0ebf98
Ashan <2017t00036@stu.cmb.ac.lk>
2025-05-15 21:55:38 +0530

    AddCourse Function implemented

--- main.c
+++ main.c
@@ -2,25 +2,28 @@
 #include <stdint.h>
 
 
-
-typedef struct {
-    char name[50];
-    uint32_t student_id;
-}Student_t;
-
 typedef struct {
     char course_name[50];
     uint32_t course_id;
 }Course_t;
+
+typedef struct {
+    char name[50];
+    uint32_t student_id;
+    Course_t course;
+}Student_t;
+
+
 
 
 
 Student_t student[50];
 Course_t course[50];
 uint32_t student_count;
+uint32_t course_count;
 
 void addStudent(Student_t *students,uint32_t *student_count);
-void addCourse(Student_t *students,uint32_t *student_count);
+void addCourse(Student_t *students,uint32_t *course_count);
 void enrollStudentInCourse(Student_t *students,uint32_t *student_count);
 void displayStudentAndCourses(Student_t *students,uint32_t *student_count);
 
@@ -45,27 +48,41 @@ void addStudent(Student_t *students,uint32_t *student_count) {
         }
     }
 
-    students[*student_count].student.student_id = student_id;
+    students[*student_count].student_id = student_id;
     getchar();
     printf("Enter student name: ");
-    scanf("%50[^\n]",&students[*student_count].student.name);
+    scanf("%50[^\n]",&students[*student_count].name);
     // students[*student_count].course.course_id = (uint32_t)NULL;
 
 }
 
-void addCourse(Students_t *students,uint32_t *student_count) {
+void addCourse(Student_t *students,uint32_t *course_count) {
+
+    uint32_t course_id = 0;
+    printf("Enter Course ID: ");
+    scanf("%d",&course_id);
+
+    for (uint32_t i = 0; i< *course_count;i++) {
+
+        if (course[i].course_id == course_id) {
+
+            printf("You can't use this ID");
+            return;
+        }
+    }
+    course[*course_count].course_id = course_id;
+    getchar();
+    printf("Enter course name: ");
+    scanf("%50[^\n]",course[*course_count].course_name);
+}
+
+void enrollStudentInCourse(Student_t *students,uint32_t *student_count) {
 
 
 
 }
 
-void enrollStudentInCourse(Students_t *students,uint32_t *student_count) {
-
-
-
-}
-
-void displayStudentAndCourses(Students_t *students,uint32_t *student_count) {
+void displayStudentAndCourses(Student_t *students,uint32_t *student_count) {
 
 
 
